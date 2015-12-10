acf067ec59a3cfca94b04ef17376c77ef01ff5fe
Stephen Long <long2know@gmail.com>
2015-12-10 10:01:28 -0500

    Setting defaults

--- routes/welcome.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(req, res){
-    var flashcardData = require("../data/flashcards.json");
-    var model={languages: flashcardData.languages};
-    res.clearCookie("flashcard-language");
-    res.render("welcome.ejs",model);
-};
