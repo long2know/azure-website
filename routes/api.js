176d2909485c7a7b74e86597546ee0843c65907a
Stephen Long <long2know@gmail.com>
2015-12-17 22:01:24 -0500

    Randomly returning errors

--- routes/api.js
+++ routes/api.js
@@ -24,17 +24,37 @@ router.get('/customer', function (req, res, next) {
 });
 
 router.post('/user', function (req, res, next) {
+    var randNumber = function() {
+        var min = 1;
+        var max = 100;
+        var random = Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    
     var firstname = req.body.firstname;
     var lastname = req.body.lastname;
     var email = req.body.email;
+    
     var error = [
         { Key: 'firstname', Value: ['Your first name is no good'] },
         { Key: 'lastname', Value: ['Last name is no good'] },
         { Key: 'email', Value: ['Email is invalid'] },
     ];
-    var errorStr = JSON.stringify(error);
-    res.writeHead(400, 'validationException', { 'content-type': 'text/plain' });
-    res.end(errorStr);
+    
+    // Randomly choose which errors to keep
+    for (var i = 0; i < error.length; i++) {
+        var rand = randNumber();
+        if (rand > 50) {
+            error.slice(i, 1);
+        }
+    }
+    
+    if (error.length > 0) {
+        var errorStr = JSON.stringify(error);
+        res.writeHead(400, 'validationException', { 'content-type': 'text/plain' });
+        res.end(errorStr);
+    } else {
+        res.end();
+    }
 });
 
 module.exports = router;
